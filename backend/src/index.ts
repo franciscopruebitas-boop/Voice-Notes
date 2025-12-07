9dd65b4f0035618307a1cda2a3c6b19ad2f42c47
Fernando <fernando@KaliFer>
2025-12-07 12:06:19 +0100

    Fix: more image maximum size

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,8 @@ const elevenlabsClient = new ElevenLabsClient({
   apiKey: process.env.ELEVENLABS_API_KEY,
 });
 
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
